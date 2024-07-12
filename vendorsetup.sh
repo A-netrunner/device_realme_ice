d0395b2ed0da33fa24b153c7bff67320f6831603
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-07-12 14:44:30 +0530

    `vendorsetup.sh` -> Move "vibrator" to local manifests

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 #Clone Vibrator
-rm -rf vendor/qcom/opensource/vibrator
-git clone https://github.com/XRP-Stuff/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
+#rm -rf vendor/qcom/opensource/vibrator
+#git clone https://github.com/XRP-Stuff/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
+
+# NOT NEEDED, AS THE REPO IS INCLUDED IN THE LOCAL MANIFESTS: https://github.com/DevInfinix/android-aosp-local-manifests
